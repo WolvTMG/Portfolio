c43fcfe05b21f8770a35ac245605fabdda14d556
WolvTMG <llvlldvytll@gmail.com>
2024-09-27 17:40:51 +0100

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,14 @@ export default function Home() {
     <div>
       {/* Meta information */}
       <Head>
-        <title>Home Page</title>
-        <meta name="description" content="This is the home page of my Next.js app" />
+        <title>TMG - Portfolio</title>
+        <meta name="TMG - Portfolio" content="Welcome to my portfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       {/* Main HTML content */}
-      <main>
-        <h1>Welcome to My Next.js App</h1>
+      <main className="text-center">
+        <h1 className="text-4xl font-bold text-orange-600">Welcome to My Next.js App</h1>
         <p>This is a simple page created with JSX in Next.js!</p>
       </main>
       
